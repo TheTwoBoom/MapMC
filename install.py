54c63d8a53b47ad2f5f51d89c0b5c0b591c9a908
TheTwoBoom <87650485+TheTwoBoom@users.noreply.github.com>
2022-05-03 17:56:27 +0200

    Delete install.py

--- install.py
+++
@@ -1,8 +0,0 @@
-def install(paket):
-    import subprocess
-    import sys
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', paket])
-def upgrade():
-    import subprocess
-    import sys
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', paket])
